c87f51bd4a457b2046ca251a066621dc72a7c485
dieisongularte <dieissongularte@gmail.com>
2021-04-01 14:48:22 -0300

    create project

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO.Cursos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+    }
+}
