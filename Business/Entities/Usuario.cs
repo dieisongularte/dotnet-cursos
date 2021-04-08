a45ce027c7500de8937da5d67e3204ce8d24a66e
dieisongularte <dieissongularte@gmail.com>
2021-04-08 19:56:24 -0300

    add entities

---
+++ Business/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace DIO.Cursos.Business.Entities
+{
+    public class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
